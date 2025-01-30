2bffedb7036106655592ac7b28324d55a58a5de5
E Dunbar <edunbar02@gmail.com>
2025-01-30 14:14:56 -0600

    Update fda analysis

--- fda/fda_approval.R
+++ fda/fda_approval.R
@@ -1,14 +1,30 @@
 #!/usr/bin/env Rscript
 
+library(dplyr)
 library(readr)
 library(stringr)
 library(tidyverse)
 
-match_drug <- function(query, reference) {
+options(tibble.print_max = Inf)
 
+match_drugs <- function(query, reference) {
+  matches <- reference %>%
+    left_join(query, by = "generic_name", suffix = c("", "_query")) %>%
+    left_join(query, by = "brand_name", suffix = c("", "_query")) %>%
+    filter(!is.na(generic_name_query) | !is.na(brand_name_query)) %>%
+    select(generic_name, brand_name) %>%
+    unique()
+
+  return(matches)
 }
 
-fda_list <- read_tsv("Products.txt") %>%
-  select(generic_name = ActiveIngredient, brand_name = DrugName)
+fda_list <- read_tsv("Products.tsv") %>%
+  select(generic_name = ActiveIngredient, brand_name = DrugName) %>%
+  mutate(across(everything(), tolower))
 drug_list <- read_csv("drug_list.csv") %>%
-  select(generic_name = `Generic Name`, brand_name = `Brand Name`)
+  select(generic_name = `Generic Name`, brand_name = `Brand Name`) %>%
+  mutate(across(everything(), tolower))
+
+matches <- match_drugs(drug_list, fda_list)
+
+matches
