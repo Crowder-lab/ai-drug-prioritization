ffc409c4029cefdb3b980633e7e60b53ad8eafde
E Dunbar <edunbar02@gmail.com>
2025-02-18 17:01:01 -0600

    Finish refactor to OO

--- src/data_augmenter.py
+++ src/data_augmenter.py
@@ -3,8 +3,8 @@ from collections.abc import Iterator
 
 import catboost as cb
 import pandas as pd
-from numpy.typing import NDArray
 from tqdm import tqdm
+from typing_extensions import Self
 
 import maplight_gnn
 
@@ -28,16 +28,58 @@ class Drugbank:
             self.queries = queries
 
     def match_cas_number(self, element: ET.Element) -> pd.Series | None:
-        maybe_cas_number = element.find("cas-number", self.namespaces)
-        return self.queries == maybe_cas_number.text if maybe_cas_number is not None else None
+        maybe_cas_number = self.cas_number(element)
+        return self.queries == maybe_cas_number if maybe_cas_number is not None else None
 
     def match_names(self, element: ET.Element) -> pd.Series:
+        generic_names, brand_names = self.names(element)
+        return self.queries[0].isin(generic_names) | self.queries[1].isin(brand_names)
+
+    def match_unii(self, element: ET.Element) -> pd.Series | None:
+        maybe_unii = self.unii(element)
+        return self.queries == maybe_unii if maybe_unii is not None else None
+
+    def get_matches(self) -> Iterator[tuple[pd.Series, ET.Element]]:
+        for _, element in tqdm(ET.iterparse(self.filename, ["end"])):
+            # inverted to reduce nesting
+            if element.tag[24:] != "drug":  # ignore the namespace
+                continue
+
+            # check to see if our element matches one of our drugs
+            matches = self.search_function(element)
+            if matches is None or not matches.any():
+                continue
+
+            # matches exist
+            yield matches, element
+
+    def cas_number(self, element: ET.Element) -> str | None:
+        maybe_cas_number = element.find("cas-number", self.namespaces)
+        return maybe_cas_number.text if maybe_cas_number is not None else None
+
+    def fda_approval(self, element: ET.Element) -> bool | None:
+        maybe_groups = element.find("groups", self.namespaces)
+        return "approved" in tuple(maybe_groups.itertext()) if maybe_groups is not None else None
+
+    def indication(self, element: ET.Element) -> str | None:
+        maybe_indication = element.find("indication", self.namespaces)
+        return maybe_indication.text if maybe_indication is not None else None
+
+    def mechanism(self, element: ET.Element) -> str | None:
+        maybe_mechanism = element.find("mechanism-of-action", self.namespaces)
+        return maybe_mechanism.text if maybe_mechanism is not None else None
+
+    def name(self, element: ET.Element) -> str | None:
+        maybe_name = element.find("name", self.namespaces)
+        return maybe_name.text if maybe_name is not None else None
+
+    def names(self, element: ET.Element) -> tuple[tuple[str], tuple[str]]:
         generic_names = set()
         brand_names = set()
         # main name
-        maybe_name = element.find("name", self.namespaces)
+        maybe_name = self.name(element)
         if maybe_name is not None:
-            generic_names.add(maybe_name.text.lower())
+            generic_names.add(maybe_name.lower())
         # generic names
         maybe_synonyms = element.find("synonyms", self.namespaces)
         if maybe_synonyms is not None:
@@ -55,41 +97,7 @@ class Drugbank:
         generic_names = tuple(filter(lambda s: "\n" not in s, generic_names))
         brand_names = tuple(brand_names)
 
-        return self.queries[0].isin(generic_names) | self.queries[1].isin(brand_names)
-
-    def match_unii(self, element: ET.Element) -> pd.Series | None:
-        maybe_unii = element.find("unii", self.namespaces)
-        return self.queries == maybe_unii.text if maybe_unii is not None else None
-
-    def get_matches(self) -> Iterator[ET.Element]:
-        for _, element in tqdm(ET.iterparse(self.filename, ["end"])):
-            # inverted to reduce nesting
-            if element.tag[24:] != "drug":  # ignore the namespace
-                continue
-
-            # check to see if our element matches one of our drugs
-            matches = self.search_function(element)
-            if matches is None or not matches.any():
-                continue
-
-            # matches exist
-            yield element
-
-    def fda_approval(self, element: ET.Element) -> bool | None:
-        maybe_groups = element.find("groups", self.namespaces)
-        return "approved" in tuple(maybe_groups.itertext()) if maybe_groups is not None else None
-
-    def indication(self, element: ET.Element) -> str | None:
-        maybe_indication = element.find("indication", self.namespaces)
-        return maybe_indication.text if maybe_indication is not None else None
-
-    def mechanism(self, element: ET.Element) -> str | None:
-        maybe_mechanism = element.find("mechanism-of-action", self.namespaces)
-        return maybe_mechanism.text if maybe_mechanism is not None else None
-
-    def name(self, element: ET.Element) -> str | None:
-        maybe_name = element.find("name", self.namespaces)
-        return maybe_name.text if maybe_name is not None else None
+        return generic_names, brand_names
 
     def prices(self, element: ET.Element) -> list[str] | None:
         maybe_prices = element.find("prices", self.namespaces)
@@ -113,50 +121,98 @@ class Drugbank:
                     smiles = property.findtext("value", namespaces=self.namespaces)
         return smiles
 
+    def unii(self, element: ET.Element) -> str | None:
+        maybe_unii = element.find("unii", self.namespaces)
+        return maybe_unii.text if maybe_unii is not None else None
+
 
 class DataAugmenter:
     def __init__(self, filename: str) -> None:
         self.filename = filename
         self.drug_list = None
+        self.admet_model = None
 
-    def load_drug_queries(self) -> pd.DataFrame:
+    def load_drug_queries(self) -> Self:
         with open(self.filename, "r") as file:
             self.drug_list = pd.read_csv(file)
+        return self
 
-        return self.drug_list
+    def load_admet_model(self, filename: str) -> Self:
+        self.admet_model = cb.CatBoostClassifier()
+        self.admet_model.load_model(filename)
+        return self
+
+    def save_drug_info(self, filename: str) -> None:
+        if self.drug_list is None:
+            raise ValueError("drug_list must be loaded first.")
+        with open(filename, "w") as file:
+            self.drug_list.to_json(file, orient="records")
 
     def match_drugbank(self, filename: str, search_type: str, queries: pd.Series | tuple[pd.Series, pd.Series]) -> None:
         if self.drug_list is None:
             raise ValueError("drug_list is not defined. Call load_drug_queries before match_drugbank.")
 
         # set up constants
+        cas_column = "CAS Registry Number"
         fda_column = "FDA Approved"
         indication_column = "Indication"
         mechanism_column = "Mechanism"
         name_column = "DrugBank Name"
         price_column = "Price"
         smiles_column = "SMILES"
+        unii_column = "UNII"
 
         # create new columns
+        self.drug_list[cas_column] = None
+        self.drug_list[fda_column] = None
+        self.drug_list[indication_column] = None
+        self.drug_list[mechanism_column] = None
         self.drug_list[name_column] = None
         self.drug_list[price_column] = self.drug_list.apply(lambda _: [], axis=1)
         self.drug_list[smiles_column] = None
+        self.drug_list[unii_column] = None
 
         drugbank = Drugbank(filename, search_type, queries)
-        for matching_element in drugbank.get_matches():
-            self.drug_list.loc[self.drug_list, fda_column] = drugbank.fda_approval(matching_element)
-            self.drug_list.loc[self.drug_list, indication_column] = drugbank.indication(matching_element)
-            self.drug_list.loc[self.drug_list, mechanism_column] = drugbank.mechanism(matching_element)
-            self.drug_list.loc[self.drug_list, price_column] = self.drug_list.loc[self.drug_list, price_column].apply(
+        for matches, matching_element in drugbank.get_matches():
+            self.drug_list.loc[matches, cas_column] = drugbank.cas_number(matching_element)
+            self.drug_list.loc[matches, fda_column] = drugbank.fda_approval(matching_element)
+            self.drug_list.loc[matches, indication_column] = drugbank.indication(matching_element)
+            self.drug_list.loc[matches, mechanism_column] = drugbank.mechanism(matching_element)
+            self.drug_list.loc[matches, name_column] = drugbank.name(matching_element)
+            self.drug_list.loc[matches, price_column] = self.drug_list.loc[matches, price_column].apply(
                 lambda _: drugbank.prices(matching_element)
             )
-            self.drug_list.loc[self.drug_list, smiles_column] = drugbank.smiles(matching_element)
+            self.drug_list.loc[matches, smiles_column] = drugbank.smiles(matching_element)
+            self.drug_list.loc[matches, unii_column] = drugbank.unii(matching_element)
 
-    def predict_bbb(self, model: cb.CatBoostClassifier, smiles: pd.Series) -> NDArray:
+    def predict_bbb(self) -> None:
+        if self.drug_list is None:
+            raise ValueError("drug_list is not defined. Call load_drug_queries before predict_bbb.")
+        if self.admet_model is None:
+            raise ValueError("admet_model is not defined. Call load_admet_model before predict_bbb.")
+        if "SMILES" not in self.drug_list.columns:
+            raise ValueError("SMILES data does not exist yet. Run match_drugbank to create it")
+
+        smiles_mask = self.drug_list["SMILES"].notna()
+        smiles = self.drug_list["SMILES"][smiles_mask]
+
         fingerprints = maplight_gnn.get_fingerprints(smiles)
-        predictions = model.predict_proba(fingerprints)
-        return predictions[:, 1]
+        predictions = self.admet_model.predict_proba(fingerprints)
+
+        self.drug_list.loc[smiles_mask, "Blood Brain Barrier"] = predictions[:, 1]
 
 
 if __name__ == "__main__":
-    pass
+    # set up augmenter
+    augmenter = (
+        DataAugmenter("data/src/cas_list.csv")
+        .load_drug_queries()
+        .load_admet_model("data/src/bbb_martins-0.916-0.002.dump")
+    )
+
+    # get data
+    augmenter.match_drugbank("data/src/drugbank.xml", "cas", augmenter.drug_list["CAS Registry Number"])
+    augmenter.predict_bbb()
+
+    # save it
+    augmenter.save_drug_info("data/drug_list.json")
